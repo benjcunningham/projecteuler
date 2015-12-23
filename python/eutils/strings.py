ff6d4ca8d0423d4618126a345525e5be36ac16fc
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-12-23 11:00:34 -0600

    Add recursive list permuter

--- python/eutils/strings.py
+++ python/eutils/strings.py
@@ -5,3 +5,10 @@ def is_palindrome(x):
 
 def map_vals(x, dval):
     return [dval[y] for y in list(x)]
+
+def l_permute(x):
+    if len(x) < 1:
+        return [[]]
+    else:
+        return [[y] + p for y in x
+                for p in l_permute([z for z in x if z != y])]
