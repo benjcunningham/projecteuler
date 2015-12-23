b8c0a1892ac33a59dbedb020d8aef2a13f282de6
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-12-22 22:53:46 -0600

    Handle vectorized math on empty lists

--- python/eutils/vectorize.py
+++ python/eutils/vectorize.py
@@ -2,7 +2,11 @@ import functools
 import operator
 
 def v_prod(x):
+    if len(x) < 1:
+        return 0
     return functools.reduce(operator.mul, x)
 
 def v_sum(x):
+    if len(x) < 1:
+        return 0
     return functools.reduce(operator.add, x)
